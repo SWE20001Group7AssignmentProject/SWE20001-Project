529a2ec65fffdc26adfdf31c7a9d1d2292fcaac3
SWE20001Group7AssignmentProject <115148070+SWE20001Group7AssignmentProject@users.noreply.github.com>
2022-11-16 23:41:14 +0800

    Generate sales report as CSV file
    Added a new function

---
+++ gensaleslogic.php
@@ -0,0 +1,39 @@
+<?php 
+// Database configuration
+$host = "localhost";
+$username = "root"; // $username = "id19669885_go2gro"; <-- username in the web application
+$password = "";
+$databasename = "id19669885_go2gro_db"; // This variable can be of any database
+
+// Connect to the database
+$conn = mysqli_connect($host, $username, $password, $databasename);
+
+$query = "SELECT * FROM salerecords";
+$result = mysqli_query($conn, $query);
+
+$num_column = mysqli_num_fields($result);		
+
+$csv_header = '';
+for($i=0;$i<$num_column;$i++) {
+    $csv_header .= '"' . mysqli_fetch_field_direct($result,$i)->name . '",';
+}	
+$csv_header .= "\n";
+
+$csv_row ='';
+while($row = mysqli_fetch_row($result)) {
+	for($i=0;$i<$num_column;$i++) {
+		$csv_row .= '"' . $row[$i] . '",';
+	}
+	$csv_row .= "\n";
+}	
+
+// Save the dumped data to a CSV file
+$t = time();
+$csvfile = 'salerecordstable_'.date("d-m-y",$t) . '.csv';
+
+/* Download as CSV File */
+header('Content-type: application/csv');
+header('Content-Disposition: attachment; filename='.basename($csvfile));
+echo $csv_header . $csv_row;
+exit;
+?>
---
+++ gensalespage.php
@@ -0,0 +1,12 @@
+<html>
+
+<head>
+<title> Download Sales Report CSV File </title>
+</head>
+
+<body>
+<h1> Download Sales Report CSV File </h1>
+<a href="gensaleslogic.php"> Download Link </a>
+</body>
+
+</html>
